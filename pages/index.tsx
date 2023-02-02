514c5bee11589cc25cf364561cda6861aa93016d
Jackson <z5397791@ad.unsw.edu.au>
2023-02-02 12:25:45 +1100

    Add paper mantine component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Inter } from "@next/font/google";
+import { Paper } from "@mantine/core";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div style={{
-    position: "static",
-    height: "100vh",
-    backgroundImage: "url('https://littlevisuals.co/images/atlantic_ridge.jpg')",
-    backgroundSize: "cover",
-   }}>
+    <div
+      style={{
+        position: "static",
+        height: "100vh",
+        backgroundImage:
+          "url('https://littlevisuals.co/images/atlantic_ridge.jpg')",
+        backgroundSize: "cover",
+      }}
+    >
+      <Paper withBorder p="lg" style={{ maxWidth: "500px" }}>
 
-   </div>
-  )
+      </Paper>
+
+    </div>
+  );
 }
